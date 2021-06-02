f422de4af468ea94c0aa32c7c3549bb343ba670d
elm1995 <>
2021-06-02 17:13:01 +0000

    added new editting

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert('WELCOME TO M3 SERIES WORLD')
-var m3answr = prompt('WHICH TYPE OF M3 YOU ADMIRE E30,E36,E46')
-alert('you like'+ m3answr)
+var m3answr = prompt('WHICH TYPE OF M3 YOU ADMIRE:  E30,E36,E46')
+alert('THNX FOR likING'+ m3answr)
 document.write('<div>' + 'hello' + "</div>")
 if (m3answr == 'E30') {
   document.write('<h3>' + m3answr + '</h3>')
