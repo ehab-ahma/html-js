0ed5fab8d914b052f02a8e21c70566e141cb12bd
elm1995 <>
2021-06-02 17:02:37 +0000

    fixing popup box

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ alert('WELCOME TO M3 SERIES WORLD')
 var m3answr = prompt('WHICH TYPE OF M3 YOU ADMIRE E30,E36,E46')
 alert('you like'+ m3answr)
 document.write('<div>' + 'hello' + "</div>")
-if (m3answr == 'e30') {
+if (m3answr == 'E30') {
   document.write('<h3>' + m3answr + '</h3>')
 }
-else if (m3answr == 'e36'){
+else if (m3answr == 'E36'){
   document.write('<h3>' + m3answr + '</h3>')
   }
-else if (m3answr=='e46'){
+else if (m3answr=='E46'){
 
 document.write('<h3>'+ m3answr + '</h3>')
 }
 else {
-  alert('you are un the wrong place')
+  alert('then you are In the wrong place')
 } 
